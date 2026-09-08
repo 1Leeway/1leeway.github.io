d05161c2c1a3fca1de564d3a2579a2b3f8cd6ca0
1Leeway <evan.goreau16@gmail.com>
2026-09-08 14:26:21 +0200

    Refactor code structure for improved readability and maintainability

---
+++ projects.js
@@ -0,0 +1,29 @@
+window.portfolioProjects = [
+  {
+    position: '01',
+    image: 'src/42.png',
+    name: '42 Piscine',
+    tags: ['C', 'Bases'],
+    description: 'Première immersion à 42 : je n’ai pas validé la Piscine, mais cette expérience m’a permis d’acquérir mes premières bases en C et en logique de programmation.',
+    visit: 'https://42angouleme.fr/',
+    year: '2026'
+  },
+  {
+    position: '02',
+    image: 'src/Arch.png',
+    name: 'Arch Linux',
+    tags: ['Linux', 'Terminal', 'KDE / GNOME'],
+    description: 'Installation et configuration d’un système Arch Linux, avec utilisation avancée du terminal et mise en place d’environnements graphiques comme KDE ou GNOME.',
+    visit: 'https://archlinux.org/',
+    year: '2023 - 2026'
+  },
+  {
+    position: '03',
+    image: 'src/Discord.png',
+    name: 'Discord Bot',
+    tags: ['JavaScript', 'API', 'Discord'],
+    description: 'Développement d’un bot Discord avec discord.js, permettant d’interagir avec les utilisateurs et d’exécuter des commandes spécifiques.',
+    visit: 'https://discord.js.org/',
+    year: '2025 - 2026'
+    },
+];
